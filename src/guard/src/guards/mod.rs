8c6cebeca6b8e48e0e6e8d848ef2fb17628d95f1
Max Murphy <max@winning.black>
2024-08-16 11:44:42 +0200

    ++

--- src/guard/src/guards/mod.rs
+++ src/guard/src/guards/mod.rs
@@ -1,16 +1,8 @@
 //! Guards for specific flows
+
+use ic_papi_api::PaymentError;
 pub mod attached_cycles;
 
 pub trait PaymentGuard {
     fn deduct(fee: u64) -> Result<(), PaymentError>;
-}
-
-#[non_exhaustive]
-pub enum PaymentError {
-    InsufficientFunds,
-}
-
-#[non_exhaustive]
-pub enum PaymentType {
-    AttachedCycles,
 }

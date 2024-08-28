24c3acdb14abedbfbb9999dc6cd2452bbd4b9059
Max Murphy <max@winning.black>
2024-08-28 15:07:38 +0200

    ++

--- src/example/tests/attached_cycles.rs
+++ src/example/tests/attached_cycles.rs
@@ -1,9 +1,9 @@
 use pocket_ic::PocketIc;
 
- #[test]
- fn test_counter_canister() {
+#[test]
+fn test_counter_canister() {
     let pic = PocketIc::new();
     // Create an empty canister as the anonymous principal and add cycles.
-    //let canister_id = pic.create_canister();
-    //pic.add_cycles(canister_id, 2_000_000_000_000);  
- }
+    let canister_id = pic.create_canister();
+    pic.add_cycles(canister_id, 2_000_000_000_000);
+}

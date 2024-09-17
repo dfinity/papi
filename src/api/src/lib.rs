ac6c40f628ddee96fbac57351e89f10f319b9426
Max <bitdivine@users.noreply.github.com>
2024-09-17 16:39:48 +0200

    refactor(api): Move code out of lib.rs (#12)
    # Motivation
    The api lib.rs is going to become one large unmanageable mess very
    quickly if all the code is in one file.
    
    
    # Changes
    - Move api definitions into smaller files.
    
    # Tests
    See CI

--- src/api/src/lib.rs
+++ src/api/src/lib.rs
@@ -1,39 +1,13 @@
-use candid::{CandidType, Deserialize, Principal};
+use candid::Principal;
 pub use cycles_ledger_client::Account;
-use cycles_ledger_client::WithdrawFromError;
 use serde_bytes::ByteBuf;
 
-#[derive(Debug, CandidType, Deserialize, Clone, Eq, PartialEq)]
-#[non_exhaustive]
-pub enum PaymentError {
-    UnsupportedPaymentType,
-    LedgerUnreachable {
-        ledger: Principal,
-    },
-    LedgerError {
-        ledger: Principal,
-        error: WithdrawFromError,
-    },
-    InsufficientFunds {
-        needed: u64,
-        available: u64,
-    },
-}
-
-#[derive(Debug, CandidType, Deserialize, Copy, Clone, Eq, PartialEq)]
-#[non_exhaustive]
-pub enum PaymentType {
-    /// The caller is paying with cycles attached to the call.
-    ///
-    /// Note: This is not available for ingress messages.
-    ///
-    /// Note: The API does not require additional arguments to support this payment type.
-    AttachedCycles,
-    /// The caller is paying with cycles from their main account on the (by default cycles) ledger.
-    CallerIcrc2,
-    /// A patron is paying, on behalf of the caller, from their main account on the (by default cycles) ledger.
-    PatronIcrc2(Principal),
-}
+pub mod caller;
+pub mod error;
+pub mod vendor;
+pub use caller::PaymentType;
+pub use error::PaymentError;
+pub use vendor::Icrc2Payer;
 
 pub fn principal2account(principal: &Principal) -> ByteBuf {
     // TODO: This is NOT the right way.
@@ -42,21 +16,4 @@ pub fn principal2account(principal: &Principal) -> ByteBuf {
         ans.push(0);
     }
     ByteBuf::from(ans)
-}
-
-/// User's payment details for an ICRC2 payment.
-#[derive(Debug, CandidType, Deserialize, Clone, Eq, PartialEq)]
-pub struct Icrc2Payer {
-    /// The customer's principal and (optionally) subaccount.
-    ///
-    /// By default, the caller's main account is used.
-    pub account: Option<Account>,
-    /// The spender, if different from the payer.
-    pub spender_subaccount: Option<serde_bytes::ByteBuf>,
-    /// The ledger canister ID.
-    ///
-    /// Note: This is included in order to improve error messages if the caller tries to use the wrong ledger.
-    pub ledger_canister_id: Option<Principal>,
-    /// Corresponds to the `created_at_time` field in ICRC2.
-    pub created_at_time: Option<u64>,
 }

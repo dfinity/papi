a180b92ca76db7d96655d36a962f1648ef56035b
Max Murphy <max@winning.black>
2024-09-18 14:01:37 +0200

    ++

--- src/guard/src/guards/any.rs
+++ src/guard/src/guards/any.rs
@@ -1,7 +1,17 @@
 //! Accepts any payment that the vendor accepts.
 
-use ic_papi_api::vendor::PaymentOption;
+use std::any::Any;
+
+use ic_papi_api::{vendor::PaymentOption, PaymentType};
+
+use super::PaymentGuard;
 
 pub struct AnyPaymentGuard<const CAP: usize>{
     pub supported: [PaymentOption;CAP],
+}
+
+impl<const CAP: usize> AnyPaymentGuard<CAP> {
+    pub fn deduct(_fee: u64, _payment: PaymentType) {
+        unimplemented!()
+    }
 }

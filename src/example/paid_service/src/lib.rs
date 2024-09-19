d88022cff299d0bd85d791e06d5e24c98016f367
Max Murphy <max@winning.black>
2024-09-19 05:03:25 +0200

    ++

--- src/example/paid_service/src/lib.rs
+++ src/example/paid_service/src/lib.rs
@@ -40,7 +40,7 @@ async fn cost_1b_icrc2_from_caller() -> Result<String, PaymentError> {
     Ok("Yes, you paid 1 billion cycles!".to_string())
 }
 
-/// An API method that requires 1 billion cycles using an ICRC-2 approve with default parameters.
+/// An API method that requires 1 billion cycles.
 #[update()]
 async fn cost_1b(payment: PaymentType) -> Result<String, PaymentError> {
     let fee = 1_000_000_000;

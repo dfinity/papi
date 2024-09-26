920d70ee24e54493749b11b33783633951904984
Max Murphy <max@winning.black>
2024-09-26 13:55:41 +0200

    ++

--- src/guard/src/guards/icrc2_tokens.rs
+++ src/guard/src/guards/icrc2_tokens.rs
@@ -8,6 +8,58 @@ use candid::{Nat, Principal};
 use cycles_ledger_client::TransferFromArgs;
 use ic_papi_api::{caller::TokenAmount, cycles::cycles_ledger_canister_id, Account};
 
+pub struct CallerPaysIcrc2TokensPaymentGuard {
+    /// The ledger for that specific token
+    pub ledger: Principal,
+    /// The caller's principal
+    pub caller: Principal,
+}
+
+impl PaymentGuard for CallerPaysIcrc2TokensPaymentGuard {
+    async fn deduct(&self, cost: TokenAmount) -> Result<(), PaymentError> {
+        cycles_ledger_client::Service(cycles_ledger_canister_id())
+            .icrc_2_transfer_from(&TransferFromArgs {
+                from: Account {
+                    owner: self.caller,
+                    subaccount: None,
+                },
+                to: Account {
+                    owner: ic_cdk::api::id(),
+                    subaccount: None,
+                },
+                amount: Nat::from(cost),
+                spender_subaccount: None,
+                created_at_time: None,
+                memo: None,
+                fee: None,
+            })
+            .await
+            .map_err(|(rejection_code, string)| {
+                eprintln!(
+                    "Failed to reach ledger canister at {}: {rejection_code:?}: {string}",
+                    cycles_ledger_canister_id()
+                );
+                PaymentError::LedgerUnreachable {
+                    ledger: cycles_ledger_canister_id(),
+                }
+            })?
+            .0
+            .map_err(|error| {
+                eprintln!(
+                    "Failed to withdraw from ledger canister at {}: {error:?}",
+                    cycles_ledger_canister_id()
+                );
+                PaymentError::LedgerTransferFromError {
+                    ledger: cycles_ledger_canister_id(),
+                    error,
+                }
+            })
+            .map(|_| ())
+    }
+}
+
+/*
+
 pub struct Icrc2TokensPaymentGuard {
     /// The ledger for that specific token.
     pub ledger: Principal,
@@ -20,7 +72,7 @@ pub struct Icrc2TokensPaymentGuard {
 }
 
 impl PaymentGuard for Icrc2TokensPaymentGuard {
-    async fn deduct(&self, fee: TokenAmount) -> Result<(), PaymentError> {
+    async fn deduct(&self, cost: TokenAmount) -> Result<(), PaymentError> {
         cycles_ledger_client::Service(cycles_ledger_canister_id())
             .icrc_2_transfer_from(&TransferFromArgs {
                 from: self.payer_account.clone(),
@@ -28,7 +80,7 @@ impl PaymentGuard for Icrc2TokensPaymentGuard {
                     owner: self.own_canister_id,
                     subaccount: None,
                 },
-                amount: Nat::from(fee),
+                amount: Nat::from(cost),
                 spender_subaccount: self.spender_subaccount.clone(),
                 created_at_time: None,
                 memo: None,
@@ -58,3 +110,4 @@ impl PaymentGuard for Icrc2TokensPaymentGuard {
             .map(|_| ())
     }
 }
+*/

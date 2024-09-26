fb6bdea0cdfca5b43f287541a8a9bf2665e9171f
Max Murphy <max@winning.black>
2024-09-26 05:22:44 +0200

    Use only the mainnet canister ID for the cycles ledger

--- src/api/src/cycles.rs
+++ src/api/src/cycles.rs
@@ -1,26 +1,31 @@
 use candid::Principal;
 
 /// The cycles ledger canister ID on mainnet.
+/// 
+/// Note: This canister ID should also be used in test environments.  The dfx.json
+/// can implement this with:
+/// ```
+/// { "canisters": {
+///   "cycles_ledger": {
+///     ...
+///     "specified_id": "um5iw-rqaaa-aaaaq-qaaba-cai",
+///     "remote": {
+///        "id": {
+///                "ic": "um5iw-rqaaa-aaaaq-qaaba-cai"
+///        }
+///     }
+///   },
+///  ...
+/// }
+/// ```
 pub const MAINNET_CYCLES_LEDGER_CANISTER_ID: &str = "um5iw-rqaaa-aaaaq-qaaba-cai";
 
-/// The cycles ledger canister ID.
-///
-/// - If a `cycles_ledger` canister is listed in `dfx.json`, the `dfx build` command will set the
-///   environment variable `CANISTER_ID_CYCLES_LEDGER` and we use this to obtain the canister ID.
-/// - Otherwise, the mainnet cycles ledger canister ID is used.
-pub const CYCLES_LEDGER_CANISTER_ID: &str =
-    if let Some(id) = option_env!("CANISTER_ID_CYCLES_LEDGER") {
-        id
-    } else {
-        MAINNET_CYCLES_LEDGER_CANISTER_ID
-    };
-
-/// The `CYCLES_LEDGER_CANISTER_ID` as a `Principal`.
+/// The `MAINNET_CYCLES_LEDGER_CANISTER_ID` as a `Principal`.
 ///
 /// # Panics
-/// - If the `CYCLES_LEDGER_CANISTER_ID` is not a valid `Principal`.
+/// - If the `MAINNET_CYCLES_LEDGER_CANISTER_ID` is not a valid `Principal`.
 #[must_use]
 pub fn cycles_ledger_canister_id() -> Principal {
-    Principal::from_text(CYCLES_LEDGER_CANISTER_ID)
+    Principal::from_text(MAINNET_CYCLES_LEDGER_CANISTER_ID)
         .expect("Invalid cycles ledger canister ID provided at compile time.")
 }

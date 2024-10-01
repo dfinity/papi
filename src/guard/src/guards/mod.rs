3472d95e28750d75bc2d662fc07bfe2ecc249564
Max <bitdivine@users.noreply.github.com>
2024-10-01 13:34:43 +0200

    refactor(guards): Remove trait used only once (#34)
    # Motivation
    The `PaymentGuard2` trait is used only once and is unlikely to be needed
    in future, so it can be eliminated.
    
    # Changes
    - Remove `PaymentGuard2`
    
    # Tests
    Existing CI should suffice

--- src/guard/src/guards/mod.rs
+++ src/guard/src/guards/mod.rs
@@ -1,6 +1,6 @@
 //! Guards for specific flows
 
-use ic_papi_api::{caller::TokenAmount, PaymentError, PaymentType};
+use ic_papi_api::{caller::TokenAmount, PaymentError};
 pub mod any;
 pub mod attached_cycles;
 pub mod caller_pays_icrc2_cycles;
@@ -11,9 +11,4 @@ pub mod patron_pays_icrc2_tokens;
 #[allow(async_fn_in_trait)]
 pub trait PaymentGuard {
     async fn deduct(&self, fee: TokenAmount) -> Result<(), PaymentError>;
-}
-
-#[allow(async_fn_in_trait)]
-pub trait PaymentGuard2 {
-    async fn deduct(&self, payment: PaymentType, fee: TokenAmount) -> Result<(), PaymentError>;
 }

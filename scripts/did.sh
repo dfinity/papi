7b6ee45138a12a8a9c7f128816b4679d9c6f9c67
Antonio Ventilii <antonio.ventilii@dfinity.org>
2026-02-20 14:53:38 +0100

    feat(wrapper): Create did files

--- scripts/did.sh
+++ scripts/did.sh
@@ -31,7 +31,7 @@ function generate_did() {
   echo "Written: $candid_file"
 }
 
-CANISTERS=(example_app_backend example_paid_service)
+CANISTERS=(ic_papi_wrapper example_app_backend example_paid_service)
 
 for canister in "${CANISTERS[@]}"; do
   generate_did "$canister"

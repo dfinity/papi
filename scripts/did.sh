5a823b6c0cf70d782eec07e418221a7c7abc518a
Antonio Ventilii <antonio.ventilii@dfinity.org>
2026-02-20 15:04:04 +0100

    refactor

--- scripts/did.sh
+++ scripts/did.sh
@@ -31,7 +31,7 @@ function generate_did() {
   echo "Written: $candid_file"
 }
 
-CANISTERS=(ic_papi_wrapper example_app_backend example_paid_service)
+CANISTERS=(example_app_backend example_paid_service ic_papi_wrapper)
 
 for canister in "${CANISTERS[@]}"; do
   generate_did "$canister"
